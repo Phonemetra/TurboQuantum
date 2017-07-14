43c1e5ad3b73ab78173818c63598a1f95df924d2
roberto73c <roberto.cesina@phonemetra.com>
2017-07-15 01:58:48 +0200

    0.0.1
    0.0.1

--- turbo/Qconfig.py
+++ turbo/Qconfig.py
@@ -4,5 +4,4 @@ config = {
   "url": 'https://quantumexperience.ng.bluemix.net/api'
 }
 
-if 'APItoken' not in locals():
-raise Exception("Please set up your access token. See Qconfig.py.")
+if 'APItoken' not in locals(): raise Exception("Please set up your access token. See Qconfig.py.")
