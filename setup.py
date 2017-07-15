e27682213080be314ca13d29c40c671125fed9e3
roberto73c <roberto.cesina@phonemetra.com>
2017-07-15 04:41:12 +0200

    0.0.1
    0.0.1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='TurboQuantum',
+    version=__version__,
+    description='TurboQuantum',
+    author='Phonemetra',
+    url='https://github.com/Phonemetra/TurboQuantum',
+    author_email='devteam@phonemetra.com')
+
+    
